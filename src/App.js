e000ed73673439b336cbfda2062c3ee77ce3c013
thenefenyak <nefenyak92@yandex.ru>
2024-02-20 21:29:56 +0300

    add components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <ul>
+        <li>hello world</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
